7fb7178bc28b34851accf38c45fefa243dfd685d
Joshua <chojoshua99@gmail.com>
2021-12-22 23:16:25 -0800

    Completed a basic ui for Etch-a-Sketch

--- script.js
+++ script.js
@@ -1,33 +1,84 @@
-/*Initial Global Variables :
-            1) container selects the 'container' div class in HTML
-            2) clearButton selects the button class 'clear-board' in HTML
-            3) eraserButton selects the button class 'eraser' in HTML
-            4) slider selects the slider range element in HTML
-            5) size selects the <p> element, sibling to (4) in HTML 
+/* ------------------ ETCH-A-SKETCH PROJECT ---------------------------
+        HOW IT WORKS:
+            1) Builds an initial grid (16x16) w/ an inherit black cursor
+            2) Adds drawing/erasing functionality to each button 
+            3) Adds Slider functionality to set size of grid (ie cursor size when drawing)
 */
+
+//Global variables to reference HTML elements
 const container = document.querySelector(`.container`);
+const buttonContainer = document.querySelectorAll(`button`);
 const clearButton = document.querySelector(".clear-board");
 const eraserButton = document.querySelector(".eraser");
 const blackButton = document.querySelector(".black");
+const colorButton = document.querySelector(".color");
+const incremButton = document.querySelector(".increment")
 const slider = document.getElementById("myRange");
 const size = document.getElementById("size");
 
-/*Intial Grid Size: 
-            1) I made the intial grid size be 16 x 16 by selecting an initial slider value
-            2) I made a variable gridSize for the functions below to use as a parameter
-            3) I made the internal HTML of size change in according to (1) to display current grid size
-*/
+//Sets initial slider value and displays grid size; keeps track of the grid size value
 slider.value = 16;
 slider.oninput = function () {
     size.innerHTML = `${this.value} x ${this.value}`;
 }
 let gridSize = slider.value;
 
+//Helper functions for each button when pressed; calls upon specific drawing/erasing effect
+let eraser = () => {draw("white");}
+let blackPrint = () => {draw();}
+let colorPrint = () => {draw("color");}
+let incremPrint = () => {draw("increm");}
 
-/* 'grid' utilizes a for loop and stylization to set the max size of each cell to 1fr
-    and to have the rows and length of the grid container be gridSize**2
-        ie) if gridSize = 16 then it makes a 16x16 grid with 256 total 'cells'
+/* Effect for each Button:
+        1) colorEffect => Adds a random color to cursor
+        2) shadeEffect => Adds black color to cursor
+        3) whitenEffect => Adds white color to cursor (essentially 'erases' other colors)
+        4) incremEffect => Adds shade of black that gets 10% darker each cell
 */
+let colorEffect = (e) => {
+    e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    e.target.style.filter=`brightness(100%)`;
+}
+let shadeEffect = (e) => {e.target.style.backgroundColor = "black";}
+let whitenEffect = (e) => {
+    e.target.style.backgroundColor = "white";
+    e.target.style.filter=`brightness(100%)`;
+}
+let incremEffect = (e) => {
+        let brightness = "";    
+        let string = e.target.style.filter;
+        //get brightness level as string
+        for(let i=0;i<string.length;i++){
+            if(!isNaN(string[i])){
+                brightness+=string[i];
+            }
+        }
+        //decrease brightness level by 10%
+        e.target.style.filter=`brightness(${brightness - 10}%)`;
+}
+
+//Depending on (above), terminates any other effect and uses current effect
+let draw = (shade = "default") => {
+    container.removeEventListener("mouseover", incremEffect);
+    container.removeEventListener("mouseover", colorEffect);
+    container.removeEventListener("mouseover", whitenEffect);
+    container.removeEventListener("mouseover", shadeEffect);
+    if(shade === "default"){
+        clear();
+        container.addEventListener("mouseover", shadeEffect);
+    }
+    else if(shade === "white") {
+        container.addEventListener("mouseover", whitenEffect);
+    }
+    else if(shade === "color"){
+        container.addEventListener("mouseover", colorEffect);
+    }
+    else if(shade === "increm"){
+        container.addEventListener("mouseover", incremEffect);
+    }
+}
+
+//Sets grid based on gridSize; makes sure each cell is same 1fr size relative to container size
 let grid = (gridParam) => {
     container.style.gridTemplateColumns = `repeat(${gridParam}, minmax(0,1fr))`;
     container.style.gridTemplateRows = `repeat(${gridParam}, minmax(0,1fr))`;
@@ -37,100 +88,36 @@ let grid = (gridParam) => {
     }
 }
 
-/* 'draw takes a gridSize value and shade value
-    1)I use a for loop to go through each cell and to apply a shadeEffect on 'hover'
-    2)I used an if loop to dictate whether it applies a whitening effect or shade effect
-        on 'hover'
-    3)The function 'eraser' is a parameter for an on-click eraser button to apply a whitening 
-        effect via (2)
-*/
-let draw = (shade = "default") => {
-    /*
-    let cell = document.getElementsByClassName("cell");
-
-    for(let i = 0; i < gridParam**2; i++){
-        if(shade === "default"){
-            cell[i].addEventListener("mouseenter", shadeEffect);
-        }
-        else {(shade === "white")
-           cell[i].addEventListener("mouseenter", whitenEffect);} 
-    }
-    */
-   if(shade === "default"){
-       console.log("shading");
-       container.removeEventListener("mouseover", whitenEffect);
-       container.addEventListener("mouseover", shadeEffect);}
-   else {
-       console.log("whiten")
-       container.removeEventListener("mouseover", shadeEffect);
-       container.addEventListener("mouseover", whitenEffect);
-   }
-}
-let shadeEffect = (e) => {
-    if(e.target.className === "cell"){
-        e.target.classList.add("shade");
-    }
-    console.log(e.target);
-}
-
-let logit = (e) => {
-    console.log(e.target.className);
-}
-
-let whitenEffect = (e) => {
-    if(e.target.className == "cell shade"){
-        e.target.classList.remove("shade");
-    }
-    console.log(e.target);
-    //eraserButton.removeEventListener("click", eraser, false);
-}
-let eraser = () => {
-    draw("white");
-}
-let blackPrint = () => {
-    draw();
-    //blackButton.removeEventListener("click", blackPrint, false);
-}
-
-/* 'clear' goes through each cell and resets their color back to 'white'
-    We want this to apply when: 
-        1) The user clicks the 'clear' button
-        2) When the user changes grid size via slider
-*/
+//clears all effects from board
 let clear = () => {
     let cell = document.getElementsByClassName("cell");
     for(let i = 0; i < cell.length; i++){
-        cell[i].classList.remove("shade");
+        cell[i].style.backgroundColor = "white";
+        cell[i].style.filter=`brightness(100%)`;
     }
 }
 
-/* 'resetBoard' resets the board by
-    1)getting a new grid size
-    2)deleting old board via 'deleteBoard' (removes all children from
-        grid container)
-    3)initializing new board with (1)
-*/
+//Implements new grid depending on new grid size
 let resetBoard = (e) => {
     let newGrid = e.target.value;
     deleteBoard(container);
     grid(newGrid);
-    draw();
 }
+
+//Deletes current board to implement (above)
 let deleteBoard = (parent) => {
     while(parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
 }
 
-/* 'play' consolidates all functionality:
-    1) Initializes beginning grid
-    2) Adds functionality to buttons e.g. ('clear' button clears board)
-*/
-
+//ACTIVE functions
 grid(gridSize);
 draw();
 clearButton.addEventListener("click", clear);
 eraserButton.addEventListener("click", eraser);
 blackButton.addEventListener("click", blackPrint);
+colorButton.addEventListener("click", colorPrint);
+incremButton.addEventListener("click", incremPrint);
 slider.addEventListener("mousedown", clear);
 slider.addEventListener("mouseup", resetBoard);
